defec28eafa60ed2585183bb2a079a7cfe08c38a
applekurdi76-lang <applekurdi76@gmail.com>
2026-03-06 01:54:37 +0300

    Add BrowserRouter for routing in main.tsx
    Wrap App component with BrowserRouter for routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ئەمە زیاد کرا
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* لێرە basename زیاد کرا بۆ ئەوەی لاپەڕە سپییەکە نەمێنێت */}
+    <BrowserRouter basename="/Kurdflux-/">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );

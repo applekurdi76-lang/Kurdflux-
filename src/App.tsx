f5a16f4c9808351fd63f59331c95653f2ca97dcd
applekurdi76-lang <applekurdi76@gmail.com>
2026-03-06 01:59:43 +0300

    Refactor App component to remove BrowserRouter
    Removed BrowserRouter from App component as it is now handled in main.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Admin from './Admin';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    // لێرە <BrowserRouter> لادرا چونکە لە ناو main.tsx بە basenameـەوە دامانناوە
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
